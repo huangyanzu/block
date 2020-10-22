841a62679f892f8458d34f3071f4804147b8faca
huangyanzu <709245577@qq.com>
2020-10-23 01:49:52 +0800

    绑定表格数据

--- block/block/ListViewController.swift
+++ block/block/ListViewController.swift
@@ -64,5 +64,23 @@ class ListViewController: UITableViewController {
         
         
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return personList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath)
+        
+        cell.textLabel?.text = personList[indexPath.row].name
+        cell.detailTextLabel?.text = personList[indexPath.row].phone
+        
+        return cell 
+        
+        
+    }
+    
+    
 
 }
